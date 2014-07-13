8d26d20cc063b4e6a5e61b8d647931615b66afb9
Jack Franklin <jack@jackfranklin.net>
2014-07-13 17:01:10 +0100

    tests pass

--- app/shared/cookieStore/cookieStoreSpec.js
+++ app/shared/cookieStore/cookieStoreSpec.js
@@ -3,32 +3,24 @@ describe('CookieStore Spec', function () {
   beforeEach(module('app'));
 
   var CookieStore, cookies;
+
   beforeEach(module(function($provide) {
-    $provide.value('cookies', cookies);
+    cookies = {};
+    $provide.value('$cookies', cookies);
   }));
 
   beforeEach(inject(function($injector) {
     CookieStore = $injector.get('CookieStore');
-    cookies = {};
   }));
-
-
-  afterEach(function() {
-    
-  });
-
 
   describe('#save()', function () {
     it('should store token into cookie', function () {
       CookieStore.save({token: '456', user_id: '123', user_name: 'oj206' });
-    
       expect(cookies).toEqual({
         userToken: '456',
         userId: '123',
         userName: 'oj206'
       });
     });
-      
   });
-
 });
--- app/shared/user-api-service/userApiServiceSpec.js
+++ app/shared/user-api-service/userApiServiceSpec.js
@@ -3,18 +3,18 @@ describe('UserApiService Spec', function() {
   beforeEach(module('app'));
 
   var UserApiService, $httpBackend, cookies;
-  
-  
   // this is how we can mock entire dependencies
   beforeEach(module(function($provide) {
-    // $provide.value('$firebase', spyFirebase.firebase);
+    cookies = {
+      userId: '123',
+      userToken: '456'
+    }
     $provide.value('$cookies', cookies);
   }));
 
   beforeEach(inject(function($injector) {
     UserApiService = $injector.get('UserApiService');
     $httpBackend = $injector.get('$httpBackend');
-    cookies = {userId: '123', userToken: '456'};
   }));
 
 
@@ -27,7 +27,7 @@ describe('UserApiService Spec', function() {
     it('should get the users details from the api', function() {
       $httpBackend.when('GET', UserApiService.ENDPOINT + '/users?token=456&userId=123').respond({});
       $httpBackend.expectGET(UserApiService.ENDPOINT + '/users?token=456&userId=123');
-      UserApiService.getUser({ userId: '123', token: '456' });
+      UserApiService.getUser();
       $httpBackend.flush();
     });
   });
