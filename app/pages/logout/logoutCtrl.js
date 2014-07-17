25c343681787ad9b0320e86bc51c46f2f9198f4f
Jack Franklin <jack@jackfranklin.net>
2014-07-17 23:51:23 +0100

    hacking around the random cookie problem

--- app/pages/logout/logoutCtrl.js
+++ app/pages/logout/logoutCtrl.js
@@ -2,6 +2,19 @@ angular.module('app')
 .controller('LogoutCtrl', function ($scope, CookieStore, $location, $http, $window) {
   $http.get('/_logout')
   .then(function(result) {
+    function deleteAllCookies() {
+      var cookies = document.cookie.split(";");
+
+      for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i];
+        var eqPos = cookie.indexOf("=");
+        var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+      }
+    }
+
+    deleteAllCookies();
+
     CookieStore.delete();
     $location.path('/');
     $window.location.reload();
--- app/shared/cookieStore/cookieStore.js
+++ app/shared/cookieStore/cookieStore.js
@@ -1,14 +1,14 @@
 angular.module('app')
-.factory('CookieStore', function ($cookies, $location) {
+.factory('CookieStore', function ($cookieStore, $location) {
 
   var cs = {};
 
   cs.save = function ($routeParams) {
 
     if ($routeParams.token && $routeParams.user_id && $routeParams.user_name) {
-      $cookies.userToken = $routeParams.token;
-      $cookies.userId = $routeParams.user_id;
-      $cookies.userName = $routeParams.user_name;
+      $cookieStore.put('userToken', $routeParams.token);
+      $cookieStore.put('userId', $routeParams.user_id);
+      $cookieStore.put('userName', $routeParams.user_name);
 
       $location.url($location.path());
     }
@@ -16,13 +16,15 @@ angular.module('app')
 
 
   cs.loggedIn = function() {
-    return !!($cookies.userToken && $cookies.userId && $cookies.userName);
+    return !!(
+      $cookieStore.get('userToken') && $cookieStore.get('userId') && $cookieStore.get('userName')
+    );
 
   };
   cs.delete = function () {
-    delete $cookies['userToken'];
-    delete $cookies['userId'];
-    delete $cookies['userName'];
+    $cookieStore.remove('userToken');
+    $cookieStore.remove('userId');
+    $cookieStore.remove('userName');
   };
 
   return cs;
