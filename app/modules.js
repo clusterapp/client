bddb549d971ab61b5aa69661397215ee805469cb
Ollie Jennings <ollie@olliejennings.co.uk>
2014-07-24 15:31:54 +0100

    fixed APIURLS so it picks up both dev environments

--- app/modules.js
+++ app/modules.js
@@ -14,6 +14,7 @@ app.factory('APIURLS', function($location) {
   return function() {
     if($location.absUrl().indexOf('localhost') > -1 ||
        $location.absUrl().indexOf('0.0.0.0') > -1 ||
+       $location.absUrl().indexOf('127.0.0.1') > -1 ||
        $location.absUrl().indexOf('http://server/') > -1) { // karma
       return 'http://0.0.0.0:3000';
     } else {
