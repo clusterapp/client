9e98fc849a8a751772679530612dfd57f7f2b1b3
Jack Franklin <jack@jackfranklin.net>
2014-07-24 12:49:16 +0100

    tweak the gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,47 +2,43 @@ var gulp = require('gulp');
 var $ = require('gulp-load-plugins')()
 
 
-gulp.task('js', function () {
+var commonFiles = [
+  'app/shared/vendor/async.js',
+  'app/shared/vendor/jquery.min.js',
+  'app/shared/vendor/angular.min.js',
+  'app/shared/vendor/moment.min.js',
+  'app/shared/vendor/*.js',
+  'app/modules.js',
+  'app/shared/**/*.js',
+];
 
-  gulp.src([
-      'app/shared/vendor/async.js',
-      'app/shared/vendor/jquery.min.js',
-      'app/shared/vendor/angular.min.js',
-      'app/shared/vendor/moment.min.js',
-      'app/shared/vendor/*.js',
-      'app/modules.js',
-      'app/shared/**/*.js',
-      '!app/shared/**/*Spec.js',
-      'app/pages/**/*.js',
-      '!app/pages/guest/*.js',
-      '!app/pages/**/*Spec.js'
-    ])
-    .pipe($.sourcemaps.init())
-    .pipe($.concat('cluster.js'))
-    // .pipe($.ngAnnotate())
-    // .pipe($.uglify())
-    .pipe($.sourcemaps.write())
-    .pipe(gulp.dest('app/assets/js'))
+gulp.task('js', function () {
+  gulp.src(commonFiles.concat([
+    '!app/shared/**/*Spec.js',
+    'app/pages/**/*.js',
+    '!app/pages/guest/*.js',
+    '!app/pages/**/*Spec.js'
+  ]))
+  .pipe($.sourcemaps.init())
+  .pipe($.concat('cluster.js'))
+  // .pipe($.ngAnnotate())
+  // .pipe($.uglify())
+  .pipe($.sourcemaps.write())
+  .pipe(gulp.dest('app/assets/js'))
 })
 
 
 
 gulp.task('guestjs', function () {
 
-  gulp.src([
-      'app/shared/vendor/jquery.min.js',
-      'app/shared/vendor/angular.min.js',
-      'app/shared/vendor/moment.min.js',
-      'app/shared/vendor/*.js',
-      'app/modules.js',
-      'app/shared/**/*.js',
-      '!app/shared/routes/*.js',
-      '!app/shared/**/*Spec.js',
-      'app/pages/**/*.js',
-      '!app/pages/logout/*.js',
-      '!app/pages/create/*.js',
-      '!app/pages/**/*Spec.js'
-  ])
+  gulp.src(commonFiles.concat([
+    '!app/shared/routes/*.js',
+    '!app/shared/**/*Spec.js',
+    'app/pages/**/*.js',
+    '!app/pages/logout/*.js',
+    '!app/pages/create/*.js',
+    '!app/pages/**/*Spec.js'
+  ]))
   .pipe($.sourcemaps.init())
   .pipe($.concat('guest.js'))
   // .pipe($.ngAnnotate())
@@ -56,8 +52,8 @@ gulp.task('guestjs', function () {
 
 gulp.task('sass', function () {
   gulp.src('app/assets/sass/cluster.sass')
-    .pipe($.rubySass({style: 'compressed', loadPath: process.cwd() + '/app/assets/sass'}))
-    .pipe(gulp.dest('app/assets/css'));
+  .pipe($.rubySass({style: 'compressed', loadPath: process.cwd() + '/app/assets/sass'}))
+  .pipe(gulp.dest('app/assets/css'));
 });
 
 
