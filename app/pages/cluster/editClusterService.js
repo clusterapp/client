8372ce124915d66553309d63069dcb1ed9aeead7
Jack Franklin <jack@jackfranklin.net>
2014-07-28 23:16:33 +0100

    such refactor

--- app/pages/cluster/editClusterService.js
+++ app/pages/cluster/editClusterService.js
@@ -1,5 +1,31 @@
 angular.module('app')
 .service('EditClusterService', function($q, UserApiService, ClusterApiService) {
+  var _updateCluster = function(options) {
+    options.progressBar.start();
+
+    var updateCallback = function(d) {
+      options.messages = options.messages || {};
+      if(d.errors && d.errors.length) {
+        options.progressBar.done();
+        options.notifier.pop('error', options.messages.errorTitle, options.messages.errorMessage || '');
+      } else {
+        options.progressBar.done();
+        options.notifier.pop('success', options.messages.successTitle, options.messages.successMessage || '');
+        (options.afterComplete || function() {})();
+      }
+    };
+    var updatedPromise = ClusterApiService.update(options.cluster.id, options.changeData);
+
+    if(options.cacheBust) {
+      updatedPromise.then(function(d) {
+        ClusterApiService.bustCache(options.cluster.id).then(updateCallback);
+      });
+    } else {
+      updatedPromise.then(updateCallback);
+    }
+    return updatedPromise;
+  };
+
   this.editAdmin = {
     userNamesForAutocomplete: function(owner) {
       var def = $q.defer();
@@ -11,93 +37,61 @@ angular.module('app')
       return def.promise;
     },
     update: function(options) {
-      var adminNames = options.adminNames;
-      var cluster = options.cluster;
-      var progressBar = options.progressBar;
-      var notifier = options.notifier;
-      progressBar.start();
-      async.map(adminNames, function(admin, callback) {
+      options.progressBar.start();
+      async.map(options.adminNames, function(admin, callback) {
         UserApiService.getUserByName(admin).then(function(user) {
           callback(null, user.id);
         });
       }, function(err, adminIds) {
-        ClusterApiService.update(cluster.id, {
+        ClusterApiService.update(options.cluster.id, {
           admins: adminIds
         }).then(function(d) {
-          progressBar.done();
-          notifier.pop('success', 'Admins updated', '');
+          options.progressBar.done();
+          options.notifier.pop('success', 'Admins updated', '');
         });
       });
     }
   };
   this.editSubreddits = {
     update: function(options) {
-      var progressBar = options.progressBar;
-      var cluster = options.cluster;
-
-      progressBar.start();
-      ClusterApiService.update(cluster.id, {
-        subreddits: options.subreddits
-      }).then(function(d) {
-        ClusterApiService.bustCache(cluster.id).then(function(d) {
-          progressBar.done();
-          options.notifier.pop('success', 'Subreddits updated', '');
-          (options.afterComplete || function() {})();
-        });
-      });
+      return _updateCluster(_.extend(options, {
+        cacheBust: true,
+        messages: {
+          successTitle: 'Subreddits updated'
+        },
+        changeData: { subreddits: options.subreddits }
+      }));
     }
   };
   this.editClusterName = {
     update: function(options) {
-      var progressBar = options.progressBar;
-
-      progressBar.start();
-      return ClusterApiService.update(options.cluster.id, {
-        name: options.name
-      }).then(function(d) {
-        if(d.errors && d.errors.length) {
-          progressBar.done();
-          options.notifier.pop('error', 'You already have a cluster with that name', '');
-        } else {
-          progressBar.done();
-          options.notifier.pop('success', 'Cluster name updated', '');
-          (options.afterComplete || function() {})();
+      return _updateCluster(_.extend(options, {
+        changeData: { name: options.name },
+        messages: {
+          successTitle: 'Cluster name updated',
+          errorTitle: 'You already have a cluster with that name'
         }
-      });
+      }));
     }
   };
   this.editSubscribers = {
     addSubscriber: function(options) {
       var newSubscribers = options.cluster.subscribers.slice(0);
       newSubscribers.push(options.newSubscriberId);
-      return this.update(_.extend(options, { subscribers: newSubscribers }));
+      return _updateCluster(_.extend(options, {
+        changeData: { subscribers: newSubscribers },
+        cacheBust: false
+      }));
     },
     removeSubscriber: function(options) {
       var subscriberIndex = options.cluster.subscribers.indexOf(options.subscriberToRemove);
       var newSubscribers = options.cluster.subscribers.slice(0);
       newSubscribers.splice(subscriberIndex, 1);
-      return this.update(_.extend(options, { subscribers: newSubscribers }));
+      return _updateCluster(_.extend(options, {
+        changeData: { subscribers: newSubscribers },
+        cacheBust: false
+      }));
     },
-
-    update: function(options) {
-      var progressBar = options.progressBar;
-
-      progressBar.start();
-      return ClusterApiService.update(options.cluster.id, {
-        subscribers: options.subscribers
-      }).then(function(d) {
-        if(d.errors && d.errors.length) {
-          console.log('update errors', d);
-          progressBar.done();
-          options.notifier.pop('error', 'An error occured when subscribing', '');
-        } else {
-          progressBar.done();
-          options.notifier.pop('success', 'Subscription to this cluster updated', '');
-          (options.afterComplete || function() {})();
-        }
-      });
-
-    }
   };
 });
 
