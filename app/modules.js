6ef1d84b5a9d72b57f67307cc24ea24a76b67b00
Jack Franklin <jack@jackfranklin.net>
2014-07-30 15:01:11 +0100

    fix api endpoint

--- app/modules.js
+++ app/modules.js
@@ -18,7 +18,7 @@ app.factory('APIURLS', function($location) {
        $location.absUrl().indexOf('http://server/') > -1) { // karma
       return 'http://127.0.0.1:3000';
     } else {
-      return 'http://clusterapp-client.herokuapp.com';
+      return 'http://clusterapp-api.herokuapp.com';
     }
   };
 
