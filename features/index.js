b510094d5a02bd112e5b2f55fe830bf79c66c77a
Jack Franklin <jack@jackfranklin.net>
2014-08-03 17:50:22 +0100

    fix feature spec

--- features/index.js
+++ features/index.js
@@ -4,7 +4,7 @@ var World = require('./support.js');
 describe('authenticating', function() {
   it('shows a welcome message', function(done) {
     World.authenticate(World.BASE_URL, function() {
-      assert.ok(World.containsText('Welcome to Cluster, jack'));
+      assert.ok(World.containsText('Welcome jack'));
       done();
     });
   });
